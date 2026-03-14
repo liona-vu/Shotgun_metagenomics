35278dded84115b26958c171d4c867d8f1450e43
liona-vu <230396061+liona-vu@users.noreply.github.com>
2026-03-14 15:22:39 -0400

    Update 02_convert.sh
    changed SRR numbers

--- scripts/02_convert.sh
+++ scripts/02_convert.sh
@@ -3,10 +3,10 @@
 cd data
 
 #to convert SRA file to fastq
-for i in {35..40}; 
+for i in {72..77}; 
 do 
   cd SRR81469${i}
-  echo "Converting SRR81469" ${i}
+  echo "Converting SRR81469"${i}
   fasterq-dump SRR81469${i}.sra
   cd ..
 done
