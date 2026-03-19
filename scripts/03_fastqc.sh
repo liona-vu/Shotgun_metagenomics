4e691edd0c0032c4e19b0c6472cee6b6568911aa
liona-vu <230396061+liona-vu@users.noreply.github.com>
2026-03-19 16:45:56 -0400

    Update 03_fastqc.sh

--- scripts/03_fastqc.sh
+++ scripts/03_fastqc.sh
@@ -7,10 +7,13 @@ module load fastqc
 cd data
 
 # Perform fastQC to check for quality
-for i in {72..77};
-do
-  cd SRR81469${i}
-  fastqc SRR81469${i}_1.fastq
-  fastqc SRR81469${i}_2.fastq
-  cd ..
+for i in {68..78}; do
+  if [[ ${i} = 69 ]]; then
+      continue
+  fi
+cd SRR81469${i}
+fastqc SRR81469${i}_1.fastq
+fastqc SRR81469${i}_2.fastq
+cd ..
+
 done
