0523c3c6b2ed46c3368146eb07688a42e4beb97b
liona-vu <230396061+liona-vu@users.noreply.github.com>
2026-03-20 16:11:52 -0400

    Update 02_convert.sh

--- scripts/02_convert.sh
+++ scripts/02_convert.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #script to convert SRA to fastq files
 
+#SBATCH --job-name=convert_SRA
+#SBATCH --time=02:00:00
+#SBATCH --account=def-cottenie
+#SBATCH --cpus-per-task=1
+#SBATCH --error=%x_%j_error.txt
+#SBATCH --output=%x_%j_output.txt
+#SBATCH --mem=10G
+
 #load module
 module load sra-toolkit
 
